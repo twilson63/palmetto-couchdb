52ec54bec7a097af9860dfcf2932cabb2eb0aac4
Tom Wilson <tom@jackhq.com>
2015-05-24 17:56:51 -0400

    changed put to post

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ module.exports = function (config) {
   var url = [config.endpoint, config.app].join('/')
   // subscribe
   follow({ db: url, include_docs: true, since: 'now' }, function (err, change) {
+    if (err) return console.log(err)
     if (change.doc.to) ee.emit(change.doc.to, change.doc)
   })
   // publish
   ee.on('send', function (event) {
-    request.put(url, { json: event }, function (e,r) {
+    request.post(url, { json: event }, function (e,r) {
       if (e) return console.log(e)
     })
   })
